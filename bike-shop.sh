b5bca7032afa7ee5691e5d83f6022aaf97adc9a4
hisahito <hisahito706@gmail.com>
2022-08-24 20:28:28 -0500

    addig more logic to the bike-shop.sh script

--- bike-shop.sh
+++ bike-shop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
 
 echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
 
@@ -13,7 +14,40 @@ MAIN_MENU(){
 }
 
 RENT_MENU(){
-  echo Rent Menu
+  # get available bikes
+  AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available=true ORDER BY bike_id")
+  
+  # if no bikes available
+  if [[ -z $AVAILABLE_BIKES ]]
+  then
+  # send to main menu
+  MAIN_MENU "Sorry, we don't have any bikes available right now."
+  else
+  #display available bikes
+  echo -e "\nHere are the bikes we have available:"
+  echo "$AVAILABLE_BIKES" | while read BIKE_ID BAR TYPE BAR SIZE
+  do
+    echo "$BIKE_ID) $SIZE\" $TYPE Bike"
+  done
+  #ask for bike to rent
+  echo -e "\nWhich one would you like to rent?"
+  read BIKE_ID_TO_RENT
+  #if input is not a number
+  if [[ ! $BIKE_ID_TO_RENT =~ ^[0-9]+$ ]]
+  then
+  #send to main menu
+  MAIN_MENU "That is not a valid bike number."
+  else
+  #get bike availability
+  BIKE_AVAILABILITY=$($PSQL "SELECT available FROM bikes WHERE bike_id=$BIKE_ID_TO_RENT AND available=true")
+  #if not available
+  if [[ -z $BIKE_AVAILABILITY ]]
+  then
+  #send to main menu
+  MAIN_MENU "That bike is not available."
+  fi
+  fi
+  fi
 }
 
 RETURN_MENU(){
